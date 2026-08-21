28a369b0c33e31a02fa6c4eb0163ac4bb176dc4f
guguhuang <guguhuang@tencent.com>
2026-08-21 14:22:50 +0800

    feat: 完成项目

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var car_scene: PackedScene = preload("res://scenes/car.tscn")
+var score: int = 0
 
 func _on_car_timer_timeout() -> void:
 	var car = car_scene.instantiate() as Area2D
@@ -9,6 +10,19 @@ func _on_car_timer_timeout() -> void:
 	$Objects.add_child(car)
 	car.connect("body_entered", go_to_title)
 
-func go_to_title(body: Node2D):
-	print('player car collision')
-	print(body)
+func go_to_title(_body: Node2D):
+	call_deferred("change_scene")
+
+
+func _on_score_timer_timeout() -> void:
+	score = score + 1
+	$CanvasLayer/Label.text = str(score)
+
+
+func _on_finish_area_2d_body_entered(_body: Node2D) -> void:
+	call_deferred("change_scene")
+	if score < Global.score:
+		Global.score = score
+
+func change_scene():
+	get_tree().change_scene_to_file("res://scenes/title.tscn")
